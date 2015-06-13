1688d8e08d9bb334938739afd54b1dcadde64f91
Joe Lea <joeleapublic@gmail.com>
2015-06-13 23:32:24 +0100

    Who knows why this works

--- src/RomanNumerals.hs
+++ src/RomanNumerals.hs
@@ -1,8 +1,29 @@
 module RomanNumerals where
+  import Data.List
+
   type Numeral = String
+  data RomanNumber = RomanNumber { number :: Int
+                                 , numeral :: Numeral
+                                 } deriving (Show, Eq)
+
+  allNumerals = [
+      RomanNumber 5 "V"
+    , RomanNumber 1 "I"]
+
+  nextRomanNumber :: Int -> RomanNumber
+  nextRomanNumber n = head $ romanNumbersLessThan n
+    where
+      romanNumbersLessThan :: Int -> [RomanNumber]
+      romanNumbersLessThan n = dropWhile (romanNumberGreaterThan n) allNumerals
+      romanNumberGreaterThan :: Int -> RomanNumber -> Bool
+      romanNumberGreaterThan n romanNumber = (number romanNumber) > n
 
   convertToRomanNumberals :: Int -> Numeral
   convertToRomanNumberals 0 = ""
   convertToRomanNumberals 1 = "I"
-  convertToRomanNumberals 2 = "II"
   convertToRomanNumberals 5 = "V"
+  convertToRomanNumberals n = nextNumeral ++ restOfTheNumerals
+    where
+      nextNumeral = convertToRomanNumberals nextNumber
+      restOfTheNumerals = convertToRomanNumberals $ n - nextNumber
+      nextNumber = number $ nextRomanNumber n
