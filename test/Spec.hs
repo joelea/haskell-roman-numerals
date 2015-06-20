0171ad3afeda5dc97a5f29396d72b20046a089f9
Joe Lea <joeleapublic@gmail.com>
2015-06-20 10:39:28 +0100

    Give suite better name

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,13 +5,14 @@ import RomanNumerals
 
 main :: IO ()
 main = hspec $ do
-  describe "Prelude.head" $ do
+  describe "RomanNumerals" $ do
     1 `convertsTo` "I"
     2 `convertsTo` "II"
     3 `convertsTo` "III"
     4 `convertsTo` "IV"
     5 `convertsTo` "V"
     7 `convertsTo` "VII"
+    8 `convertsTo` "Not a numeral"
 
 convertsTo number numeral =
   it ("converts " ++ (show number) ++ " -> " ++ numeral) $ do
